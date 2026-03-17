653c1f525c04dab52362187af8e9f289a54873ed
Muhammad Ishaq <ishaq18310@abasyn.edu.pk>
2026-03-18 03:42:11 +0500

    Day 3: AuthViewModel with login and register

--- lib/features/auth/auth_viewmodel.dart
+++ lib/features/auth/auth_viewmodel.dart
@@ -1,13 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:rozgarlink/data/repositories/auth_repository.dart';
 
-class AuthViewmodel {
+class AuthViewmodel extends ChangeNotifier {
+  final AuthRepository _repository = AuthRepository();
+  bool isloading = false;
+  String? errorMessage;
+
   final TextEditingController emailcontroller = TextEditingController();
   final TextEditingController passwordcontroller = TextEditingController();
-  void login() {
-    String email = emailcontroller.text;
-    String password = passwordcontroller.text;
 
-    print(email);
-    print(password);
+  // login function
+
+  Future<void> login(BuildContext context) async {
+    isloading = true;
+    errorMessage = null;
+    notifyListeners();
+
+    // repositry
+
+    String? error = await _repository.login(
+      email: emailcontroller.text.trim(),
+      password: passwordcontroller.text.trim(),
+    );
+
+    // hide lodaing
+
+    isloading = false;
+    notifyListeners();
+
+    // check result
+
+    if (error == null) {
+      String uid = _repository.getCurrentUser()!.uid;
+      String? role = await _repository.getUserRole(uid);
+
+      // navigate
+
+      if (role == 'labor') {
+        Navigator.pushReplacementNamed(context, '/laborHome');
+      } else {
+        Navigator.pushReplacementNamed(context, '/clientHome');
+      }
+    } else {
+      errorMessage = error;
+      notifyListeners();
+    }
+  }
+
+  Future<void> register(
+    BuildContext context, {
+    required String name,
+    required String phone,
+    required String skill,
+    required String city,
+    required String wage,
+  }) async {
+    isloading = true;
+    errorMessage = null;
+    notifyListeners();
+    String? error = await _repository.registerLabor(
+      email: emailcontroller.text.trim(),
+      password: passwordcontroller.text.trim(),
+      name: name,
+      phone: phone,
+      skill: skill,
+      city: city,
+      wage: wage,
+    );
+
+    isloading = false;
+    notifyListeners();
+
+    if (error == null) {
+      // Success! Go to labor home
+      Navigator.pushReplacementNamed(context, '/laborHome');
+    } else {
+      errorMessage = error;
+      notifyListeners();
+    }
   }
 }
